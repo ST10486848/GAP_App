4da11c7967c96932f43b75373ef8d76bafd790b5
st10476645 <alexejemimar@gmail.com>
2025-10-07 15:49:28 +0200

    fixed errors and added add to cart button

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -7,9 +7,8 @@ import {
     LayoutAnimation,
     TouchableOpacity,
     Button,
-    Image,
-    Pressable,
-} from "react-native";
+    Image,} 
+from "react-native";
 import styles from "../styles/Styles"; // Importing the styles from the Styles.tsx file
 
 export default function HomePage({ navigation }: { navigation: any }) {
@@ -35,8 +34,8 @@ export default function HomePage({ navigation }: { navigation: any }) {
     >
       {/* Logo and Organization Info */}
       <View style={styles.header}>
-        <View style={styles.logoContainer}>
-          <Text style={styles.logoText}>Organization Logo</Text>{" "}
+        <View style={styles.logoContainer }>
+          <Text style={styles.logoText}>Organization Logo</Text>
           {/* Replace with actual logo image */}
           {/* <Image source={logo} style={styles.logo} /> */}
         </View>
@@ -44,7 +43,7 @@ export default function HomePage({ navigation }: { navigation: any }) {
         <Text style={styles.description}>
           Founded in 2005, our organization has been dedicated to providing
           quality education and skill development programs to communities across
-          SouthAfrica. We believe in empowering individuals through practical
+          South Africa. We believe in empowering individuals through practical
           knowledge and hands-on training.
         </Text>
         <Text style={styles.description}>
@@ -63,17 +62,17 @@ export default function HomePage({ navigation }: { navigation: any }) {
             style={styles.categoryHeader}
             onPress={LongCoursesClick}
           >
-            <Text style={styles.categoryTitle}>Long Term Courses</Text>{" "}
+            <Text style={styles.categoryTitle}>Long Term Courses</Text>
             {/* This section reprsent the long courses button */}
             <Text style={styles.arrow}>
-              {showLongCourses ? "▼" : "►"}{" "}
+              {showLongCourses ? "▼" : "►"}
               {/* This line changes the arrow direction based on the state of the button */}
             </Text>
           </TouchableOpacity>
 
           {showLongCourses && ( // This line first checks if the state od the button is true (basically open) then shows the courses
 // This block of code represents the courses under the long term courses section
-            <View style={styles.courseList}>
+            <View style={styles.coursesButtons}>
               <Button
                 title="First Aid"
                 onPress={() => {navigation.navigate("FirstAid");}}
@@ -106,7 +105,7 @@ export default function HomePage({ navigation }: { navigation: any }) {
 
           {showShortCourses && (
 // This block of code represents the courses under the short term courses section 
-            <View style={styles.courseList}>
+            <View style={styles.coursesButtons}>
               <Button
                 title="Child Minding"
                 onPress={() => {navigation.navigate("ChildMinding"); }}
